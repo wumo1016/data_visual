a000a251e61625ad0d179f2f1bd167439497ec28
wumo1016 <760478279@qq.com>
2020-12-31 14:52:43 +0800

    update(p-7)

--- src/7.data-report-project/src/public/tool.js
+++ src/7.data-report-project/src/public/tool.js
@@ -3,5 +3,5 @@ const a = 12345678910
 console.log(a.toLocaleString()) // 12,345,678,910
 
 const b = '1234567890'
-const reg = /\d{1,3}(?=(\d{3})+$)/g
+const reg = /\d{1,3}(?=(\d{3})+$)/g // 反向引用 先保证右边是3个一组的数字
 console.log(b.replace(reg, '$&,')) // $&表示当前匹配到的值 1,234,567,890
